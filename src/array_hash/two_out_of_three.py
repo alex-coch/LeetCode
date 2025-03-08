c817954ce848ec934fd6e355f9873c5140023ed4
Aleksei Kokorev <kokorev.a@mybook.ru>
2025-03-08 12:02:57 +0300

    two_out_of_three.py

---
+++ src/array_hash/two_out_of_three.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoOutOfThree(self, nums1: list[int], nums2: list[int], nums3: list[int]) -> list[int]:
+        # time O(n1+n2+n3) , space O(u)
+        hm1 = set(nums1)
+        hm2 = set(nums2)
+        hm3 = set(nums3)
+        hm12 = hm1.intersection(hm2)
+        hm23 = hm2.intersection(hm3)
+        hm13 = hm1.intersection(hm3)
+        return list(hm12 | hm23 | hm13)
+
