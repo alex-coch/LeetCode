9ae55b1317984afa051c9ad844d8e9580bfa4299
Aleksei Kokorev <kokorev.a@mybook.ru>
2025-10-20 08:38:25 +0400

    container

---
+++ src/two_pointers/container.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxArea(self, height: list[int]) -> int:
+        left, right = 0, len(height) - 1
+        result = 0
+        while left < right:
+            area = min(height[left], height[right]) * (right - left)
+            result = max(result, area)
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+        return result
+
+print(Solution().maxArea([1,8,6,2,5,4,8,3,7]))
