4495a4b90a8cbd3171ae482502ec3dd89b12896e
Aleksei Kokorev <kokorev.a@mybook.ru>
2026-07-16 08:32:14 +0400

    construct

---
+++ src/trees/construct.py
@@ -0,0 +1,8 @@
+from typing import List, Optional
+
+from src.trees.support import TreeNode
+
+
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        ...
